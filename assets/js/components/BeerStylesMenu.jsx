3d3cbbd95fd7937ce2c78a8870db38c5d17b487a
Mathis Favier <favier.mathis@gmail.com>
2022-01-22 17:36:02 +0100

    Added a route in the MainController to acces the api, and it's twig file make an UML graph on how to represent a Mash

--- assets/js/components/BeerStylesMenu.jsx
+++ assets/js/components/BeerStylesMenu.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import data from '../../../src/Sql/styles.json';
 
 const BeerStylesMenu = () => {
-
   return (
     <div>
       <p>coucou</p>
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,13 +12,22 @@ class MainController extends AbstractController
 {
     /**
      * @Template("main/index.html.twig")
-     * @Route("/{reactRouting}", name="index", requirements={"reactRouting"=".+"}, defaults={"reactRouting": null})
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         $this->getUser()->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
 
         return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Template("main/dasboard.html.twig")
+     * @Route("/dashboard{reactRouting}", name="dashboard", requirements={"reactRouting"=".+"}, defaults={"reactRouting": null})
+     */
+    public function dashboard(): Response
+    {
+        return $this->render('main/dasboard.html.twig');
     }
 
 }
