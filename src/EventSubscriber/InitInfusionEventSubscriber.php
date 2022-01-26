a330db8b9b50908fa187369ecaec7b2464c42211
Mathis Favier <favier.mathis@gmail.com>
2022-01-26 15:26:55 +0100

    Same as 2 previous commit beut for InitInfusion Entity instead

---
+++ src/EventSubscriber/InitInfusionEventSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\InitInfusion;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authorization\Strategy\PriorityStrategy;
+
+class InitInfusionEventSubscriber implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+          KernelEvents::VIEW => ['computeValues', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function computeValues(ViewEvent $event)
+    {
+        if ($event->getControllerResult() instanceof InitInfusion) {
+            // G15 = (0,41/G14)*(G13-G12)+G13
+            $initInfusion = $event->getControllerResult();
+            $initInfusion->setWaterTempToAdjunct(
+                (0.41 / $initInfusion->getWaterGrainRatio()) * ($initInfusion->getWantedMashTemp() - $initInfusion->getInitGrainTemp(
+                    )) + $initInfusion->getWantedMashTemp()
+            );
+        }
+    }
+}
